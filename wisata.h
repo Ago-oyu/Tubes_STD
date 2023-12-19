d726c21f95d98683b54bde1a06eb12b47e5bcc3c
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2023-12-20 00:11:44 +0700

    buat ADT

--- wisata.h
+++ wisata.h
@@ -1,12 +1,51 @@
 #ifndef WISATA_H_INCLUDED
 #define WISATA_H_INCLUDED
 #include <iostream>
-#define info(P)(P)->info
-#define next(P)(P)->next
-#define first(L)(L).first
-#define last(L)(L).last
+using namespace std;
 
+typedef struct paketWisata *adrPaket;
+typedef struct elmRelasi *adrRelasi;
+typedef struct wisatawan *adrWisatawan;
 
+struct wisatawan {
+    string nama;
+    int umur;
 
+};
+
+struct paketWisata {
+    string destinasi;
+    int harga;
+
+};
+
+struct elmRelasi {
+    adrPaket paket;
+    adrRelasi next;
+};
+
+struct elmWisatawan {
+    wisatawan info;
+    listRelasi relasi;
+    adrWisatawan next;
+};
+
+struct elmPaketWisata {
+    paketWisata info;
+    adrPaket next;
+};
+
+struct listWisatawan {
+    adrWisatawan first;
+    adrWisatawan last;
+};
+
+struct listPaket {
+    adrPaket first;
+};
+
+struct listRelasi {
+    adrRelasi first;
+};
 
 #endif // WISATA_H_INCLUDED
