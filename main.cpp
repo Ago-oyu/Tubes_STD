8758ee3272e2e647ecf7a9829c715619f96fbd3c
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2023-12-30 00:00:21 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,35 +8,83 @@ int main()
     createListPaket(LP);
     createListWisatawan(LW);
     wisatawan W;
+    adrWisatawan pW;
     paketWisata P;
 
-    W.atasNama= "tes";
-    W.jmlDewasa = 18;
-    W.jmlAnak = 3;
+    int pilihan;
 
-    P.destinasi = "Bromo";
-    P.harga = 1000000;
-    P.durasiHari = 4;
-    P.durasiMalam = 3;
-    insertLastPaket(LP, createElmPaket(P));
-    P.destinasi = "Malang";
-    insertLastPaket(LP, createElmPaket(P));
-    showAllPaket(LP);
+    do {
+        cout<<"1. Insert Data"<<endl;
+        cout<<"2. Show Data"<<endl;
+        cout<<"3. Find Data"<<endl;
+        cout<<"4. Delete Data"<<endl;
+        cout<<"5. Connect Data"<<endl;
+        cout<<"0. Exit Program"<<endl;
+        cout<<"Pilih Menu: ";cin>>pilihan;
 
-    adrWisatawan pW = createElmWisatawan(W);
-    insertLastWisatawan(LW, pW);
-    //showAllWisatawan(LW);
+    switch(pilihan){
+        case 1:
+            cout<<"1. Insert Wisatawan"<<endl;
+            cout<<"2. Insert Paket Wisata"<<endl;
+            cout<<"Pilih Menu: ";cin>>pilihan;
+            switch(pilihan) {
+                case 1:
+                    while (W.atasNama != "#") {
+                        cout<<"Atas Nama: ";cin>>W.atasNama;
+                        cout<<"Jumlah Dewasa: ";cin>>W.jmlDewasa;
+                        cout<<"Jumlah Anak(<12 Tahun): ";cin>>W.jmlAnak;
+                        pW = createElmWisatawan(W);
+                        insertLastWisatawan(LW, pW);
+                    }
+                    break;
+                case 2:
+                    break;
+            }
+            break;
+        case 2:
+            showAllWisatawan(LW);
+//            cout<<"1. Data Wisatawan"<<endl;
+//            cout<<"Pilih Menu: ";cin>>pilihan;
 
-    W.atasNama = "tes2";
-    W.jmlDewasa = 19;
-    W.jmlAnak = 10;
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 0:
+            break;
+    }
+    } while (pilihan != 0);
 
-    //pW = createElmWisatawan(W);
-    insertLastWisatawan(LW, createElmWisatawan(W));
-    showAllWisatawanWithRelation(LW);
-
-    setPaketForWisatawan(LW, LP, "tes2", "Bromo");
-    setPaketForWisatawan(LW, LP, "tes2", "Malang");
-    showAllWisatawanWithRelation(LW);
+//    W.atasNama= "tes";
+//    W.jmlDewasa = 18;
+//    W.jmlAnak = 3;
+//
+//    P.destinasi = "Bromo";
+//    P.harga = 1000000;
+//    P.durasiHari = 4;
+//    P.durasiMalam = 3;
+//    insertLastPaket(LP, createElmPaket(P));
+//    P.destinasi = "Malang";
+//    insertLastPaket(LP, createElmPaket(P));
+//    showAllPaket(LP);
+//
+//    adrWisatawan pW = createElmWisatawan(W);
+//    insertLastWisatawan(LW, pW);
+//    //showAllWisatawan(LW);
+//
+//    W.atasNama = "tes2";
+//    W.jmlDewasa = 19;
+//    W.jmlAnak = 10;
+//
+//    //pW = createElmWisatawan(W);
+//    insertLastWisatawan(LW, createElmWisatawan(W));
+//    showAllWisatawanWithRelation(LW);
+//
+//    setPaketForWisatawan(LW, LP, "tes2", "Bromo");
+//    setPaketForWisatawan(LW, LP, "tes2", "Malang");
+//    showAllWisatawanWithRelation(LW);
     return 0;
 }
